a0d1de79a80e5af79b51440c636bc3abe39ebab1
Felix <xuanphuc.a1gv@gmail.com>
2026-01-11 04:00:49 -0500

    finish functional programming

--- 07_python_fp/sum_types/csvexport/main.py
+++ 07_python_fp/sum_types/csvexport/main.py
@@ -8,7 +8,26 @@ class CSVExportStatus(Enum):
     FAILURE = 4
 
 
-def get_csv_status(status, data):
+def get_csv_status(status: CSVExportStatus, data: list):
     match status:
         case CSVExportStatus.PENDING:
-            return
+            return (
+                "Pending...",
+                [list(map(lambda item: str(item), lst)) for lst in data],
+            )
+        case CSVExportStatus.PROCESSING:
+            commas_join = map(lambda lst: ",".join(lst), data)
+            newlines_join = "\n".join(commas_join)
+            return ("Processing...", newlines_join)
+        case CSVExportStatus.SUCCESS:
+            return ("Success!", data)
+        case CSVExportStatus.FAILURE:
+            return (
+                "Unknown error, retrying...",
+                get_csv_status(
+                    CSVExportStatus.PROCESSING,
+                    get_csv_status(CSVExportStatus.PENDING, data)[1],
+                )[1],
+            )
+        case _:
+            raise Exception("unknown export status")
