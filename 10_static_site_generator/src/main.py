c4d14bf4f27e192a0da150c47e9075ec6d00f516
felix <xuanphuc.a1gv@gmail.com>
2026-01-12 15:53:58 -0500

    move text_node conversion to utils.py

--- 10_static_site_generator/src/main.py
+++ 10_static_site_generator/src/main.py
@@ -1,27 +1,4 @@
 from textnode import TextNode, TextType
-from htmlnode import LeafNode
-
-
-def text_node_to_html_node(text_node: TextNode) -> LeafNode:
-    match text_node.text_type:
-        case TextType.TEXT:
-            return LeafNode(tag=None, value=text_node.text)
-        case TextType.BOLD:
-            return LeafNode(tag="b", value=text_node.text)
-        case TextType.ITALIC:
-            return LeafNode(tag="i", value=text_node.text)
-        case TextType.CODE:
-            return LeafNode(tag="code", value=text_node.text)
-        case TextType.LINK:
-            return LeafNode(
-                tag="a", value=text_node.text, props={"href": text_node.url}
-            )
-        case TextType.IMAGE:
-            return LeafNode(
-                tag="img", value="", props={"alt": text_node.text, "src": text_node.url}
-            )
-        case _:
-            raise Exception("Error: Invalid text_type conversion")
 
 
 def main():
--- 10_static_site_generator/src/unittest/test_textnode.py
+++ 10_static_site_generator/src/unittest/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from htmlnode import LeafNode
+from utils import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -35,35 +35,35 @@ class TestTextNode(unittest.TestCase):
 class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text_type_text(self):
         node = TextNode("This is plain text", TextType.TEXT)
-        html_node = node.text_node_to_html_node()
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is plain text")
         self.assertEqual(html_node.to_html(), "This is plain text")
 
     def test_text_type_bold(self):
         node = TextNode("Bold text", TextType.BOLD)
-        html_node = node.text_node_to_html_node()
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "Bold text")
         self.assertEqual(html_node.to_html(), "<b>Bold text</b>")
 
     def test_text_type_italic(self):
         node = TextNode("Italic text", TextType.ITALIC)
-        html_node = node.text_node_to_html_node()
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "Italic text")
         self.assertEqual(html_node.to_html(), "<i>Italic text</i>")
 
     def test_text_type_code(self):
         node = TextNode("print('Hello')", TextType.CODE)
-        html_node = node.text_node_to_html_node()
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "print('Hello')")
         self.assertEqual(html_node.to_html(), "<code>print('Hello')</code>")
 
     def test_text_type_link(self):
         node = TextNode("Click here", TextType.LINK, "https://www.boot.dev")
-        html_node = node.text_node_to_html_node()
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "Click here")
         self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
@@ -76,7 +76,7 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         node = TextNode(
             "Alt text for image", TextType.IMAGE, "https://example.com/image.png"
         )
-        html_node = node.text_node_to_html_node()
+        html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
         self.assertEqual(
