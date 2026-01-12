17415a1e5dc9bcbde8d4fe10f2296bc7fea52ad7
felix <xuanphuc.a1gv@gmail.com>
2026-01-12 09:34:27 -0500

    10: textnode

--- 10_static_site_generator/src/main.py
+++ 10_static_site_generator/src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    link_node = TextNode("Anchor text", TextType.LINK, "https://www.boot.dev")
+    print(link_node)
+
+
+if __name__ == "__main__":
+    main()
--- 10_static_site_generator/src/textnode.py
+++ 10_static_site_generator/src/textnode.py
@@ -1,8 +1,27 @@
 from enum import Enum
 
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    TEXT = ""
+    BOLD = "water"
+    ITALIC = "earth"
+    CODE = "fire"
+    LINK = "fire"
+    IMAGE = "fire"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: TextNode) -> bool:
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url}"
