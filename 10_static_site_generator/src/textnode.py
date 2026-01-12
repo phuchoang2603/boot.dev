b6b5f29854bf9a66e23c5d363dbcbd9b780e4ca7
felix <xuanphuc.a1gv@gmail.com>
2026-01-12 15:39:50 -0500

    add textnode to htmlnode conversion

--- 10_static_site_generator/src/textnode.py
+++ 10_static_site_generator/src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -25,3 +26,24 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def text_node_to_html_node(self) -> LeafNode:
+        match self.text_type:
+            case TextType.TEXT:
+                return LeafNode(tag=None, value=self.text)
+            case TextType.BOLD:
+                return LeafNode(tag="b", value=self.text)
+            case TextType.ITALIC:
+                return LeafNode(tag="i", value=self.text)
+            case TextType.CODE:
+                return LeafNode(tag="code", value=self.text)
+            case TextType.LINK:
+                return LeafNode(tag="a", value=self.text, props={"href": self.url})
+            case TextType.IMAGE:
+                return LeafNode(
+                    tag="img",
+                    value="",
+                    props={"alt": self.text, "src": self.url},
+                )
+            case _:
+                raise Exception("Error: Invalid text_type conversion")
