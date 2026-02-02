10cabc1c2a6fe233e3279d5bdb1c770c6b8ef24b
felix <xuanphuc.a1gv@gmail.com>
2026-02-02 00:07:57 -0500

    finish channels

---
+++ 13_go/channels/ping_pong/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pingPong(numPings int) {
+	pings := make(chan struct{})
+	pongs := make(chan struct{})
+
+	go ponger(pings, pongs)
+	go pinger(pings, numPings)
+
+	for range pongs {
+		fmt.Println("got pong")
+	}
+	fmt.Println("pongs done")
+}
+
+func pinger(pings chan struct{}, numPings int) {
+	sleepTime := 50 * time.Millisecond
+	for i := 0; i < numPings; i++ {
+		fmt.Printf("sending ping %v\n", i)
+		pings <- struct{}{}
+		time.Sleep(sleepTime)
+		sleepTime *= 2
+	}
+	close(pings)
+}
+
+func ponger(pings, pongs chan struct{}) {
+	i := 0
+	for range pings {
+		fmt.Printf("got ping %v, sending pong %v\n", i, i)
+		pongs <- struct{}{}
+		i++
+	}
+	fmt.Println("pings done")
+	close(pongs)
+}
+
+func test(numPings int) {
+	fmt.Println("Starting game...")
+	pingPong(numPings)
+	fmt.Println("===== Game over =====")
+}
+
+func main() {
+	test(4)
+	test(3)
+	test(2)
+}
