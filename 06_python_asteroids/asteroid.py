038e99d111e801e2f0b7235b7ceb308cdcb2fa76
felix <xuanphuc.a1gv@gmail.com>
2026-03-04 14:14:12 -0500

    finish python-asteroids
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- 06_python_asteroids/asteroid.py
+++ 06_python_asteroids/asteroid.py
@@ -1,6 +1,8 @@
+import random
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -14,3 +16,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        new_angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new1.velocity = self.velocity.rotate(new_angle) * 1.2
+
+        new2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new2.velocity = self.velocity.rotate(-new_angle) * 1.2
--- 06_python_asteroids/main.py
+++ 06_python_asteroids/main.py
@@ -48,7 +48,7 @@ def main():
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         for item in drawable:
             item.draw(screen)
