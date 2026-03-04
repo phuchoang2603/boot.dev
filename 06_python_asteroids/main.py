a1579fa94548562b508921eed564e1b9dc48dcbf
felix <xuanphuc.a1gv@gmail.com>
2026-03-04 12:59:39 -0500

    python-asteroids: render every object
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- 06_python_asteroids/main.py
+++ 06_python_asteroids/main.py
@@ -1,22 +1,44 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # pyright: ignore[reportUnusedVariable]
+    asteroid_field = AsteroidField()  # pyright: ignore[reportUnusedVariable]
+    dt = 0
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
