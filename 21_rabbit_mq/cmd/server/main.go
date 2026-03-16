2ae2fb104c4415d49f757b9020892377dc446cf5
felix <xuanphuc.a1gv@gmail.com>
2026-03-16 10:20:23 -0400

    21: repl server
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

--- 21_rabbit_mq/cmd/server/main.go
+++ 21_rabbit_mq/cmd/server/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -18,22 +18,47 @@ func main() {
 	}
 	defer conn.Close()
 
-	amqpChan, err := conn.Channel()
+	publishCh, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("Failed to open a channel: %v", err)
 	}
 
-	pubsub.PublishJSON(
-		amqpChan,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
+	gamelogic.PrintServerHelp()
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	log.Println("Received interrupt signal, shutting down gracefully...")
+	for {
+		inputs := gamelogic.GetInput()
+		if len(inputs) == 0 {
+			continue
+		}
+
+		switch inputs[0] {
+		case "pause":
+			fmt.Println("Pausing game...")
+			pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+		case "resume":
+			fmt.Println("Resuming game...")
+			pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+		case "quit":
+			fmt.Println("Quitting game...")
+			return
+		case "help":
+			gamelogic.PrintServerHelp()
+		default:
+			fmt.Printf("Unknown command: %s\n", inputs[0])
+		}
+	}
 }
