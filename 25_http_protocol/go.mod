5c76e08f3714d774e0628f27a8dfc80798c4a6dc
felix <xuanphuc.a1gv@gmail.com>
2026-03-20 15:09:16 -0400

    25: try out http protocol
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

---
+++ 25_http_protocol/go.mod
@@ -0,0 +1,3 @@
+module github.com/phuchoang2603/boot.dev/25_http_protocol
+
+go 1.25.5
---
+++ 25_http_protocol/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		data := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", string(data[:n]))
+	}
+}
